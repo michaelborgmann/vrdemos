c10bda46c722f6ed5cf42999e433aa7bdbe5a7fc
Michael Borgmann <michaelborgmann@mailbox.org>
2017-03-20 08:56:41 +0100

    Initial Commit

---
+++ VR Demos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VR Demos
+//
+//  Created by Michael Borgmann on 20/03/2017.
+//  Copyright © 2017 Michael Borgmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
