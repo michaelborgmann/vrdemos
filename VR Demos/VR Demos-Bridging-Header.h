611491f04bde8c76443e799c9497bfccad58087f
Michael Borgmann <michaelborgmann@mailbox.org>
2017-03-21 20:07:35 +0100

    app base for demo - tab bar view controller - panorama, video, cardboard view - tab bar icons

---
+++ VR Demos/VR Demos-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  VR Demos-Bridging-Header.h
+//  VR Demos
+//
+//  Created by Michael Borgmann on 20/03/2017.
+//  Copyright © 2017 Michael Borgmann. All rights reserved.
+//
+
+#ifndef VR_Demos_Bridging_Header_h
+#define VR_Demos_Bridging_Header_h
+
+// Google Cardboard VR SDK
+#import "GVRPanoramaView.h"
+#import "GVRWidgetView.h"
+#import "GVRVideoView.h"
+
+#endif /* VR_Demos_Bridging_Header_h */
